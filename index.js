93e0e568a771faad00fbe7f3bb19f7495bfa7da7
James Rees <jarees@gmail.com>
2017-09-20 15:42:05 +1000

    use PORT env

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-const port = 8000;
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+app.listen(app.get('port'), () => {
+  console.log('We are live on ' + app.get('port'));
 });
